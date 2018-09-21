d6032c0c1dc9f9b3daf4c4132410fa3418120499
sflemingo <spencer@fleming.net>
2018-09-21 17:35:08 -0400

    added a very simple command line UI for the user

---
+++ src/view/kinematicsUI.java
@@ -0,0 +1,116 @@
+/*
+ * kinematicsUI class
+ * view of the KinematicsCalculator
+ *
+ * very simple user interface for solving kinematics problems,
+ * will probably implement javafx later.
+ *
+ * Variables are:
+ * a = acceleration in meters/second^2
+ * v0 = initial velocity in meters/second
+ * v = velocity in meters/second
+ * x0 = initial displacement in meters
+ * x = displacement in meters
+ * t = time in seconds
+ *
+ * run this with the known variables as arguments in any order like this example:
+ * ~$ java kinematicsUI t:2.3 a:-9.8 v0:16.2
+ *
+ * */
+package view;
+
+import controller.kinematicsController;
+
+public class kinematicsUI {
+
+    /*
+     * main method for the kinematicsUI
+     * */
+    public static void main(String args[]){
+        kinematicsController kC = new kinematicsController();
+
+        kC.setAcceleration(null);
+        kC.setInitialVelocity(null);
+        kC.setVelocity(null);
+        kC.setInitialDisplacement(null);
+        kC.setDisplacement(null);
+        kC.setTime(null);
+
+        for (int i = 0; i < args.length; i++){
+            if (args[i].startsWith("a:"))
+                kC.setAcceleration(Double.valueOf(args[i].substring(2)));
+            if (args[i].startsWith("v:"))
+                kC.setVelocity(Double.valueOf(args[i].substring(2)));
+            if (args[i].startsWith("v0:"))
+                kC.setInitialVelocity(Double.valueOf(args[i].substring(3)));
+            if (args[i].startsWith("x:"))
+                kC.setDisplacement(Double.valueOf(args[i].substring(2)));
+            if (args[i].startsWith("x0:"))
+                kC.setInitialDisplacement(Double.valueOf(args[i].substring(3)));
+            if (args[i].startsWith("t:"))
+                kC.setTime(Double.valueOf(args[i].substring(2)));
+        }
+
+        kC.solve();
+
+        System.out.println("a:\t" + kC.getVariable("a"));
+        System.out.println("v0:\t" + kC.getVariable("v0"));
+        System.out.println("v:\t" + kC.getVariable("v"));
+        System.out.println("x0:\t" + kC.getVariable("x0"));
+        System.out.println("x:\t" + kC.getVariable("x"));
+        System.out.println("t:\t" + kC.getVariable("t"));
+
+        System.out.println();
+        boolean useAnd = false;
+        if(kC.getVariable("x0") != null || kC.getVariable("v0") != null || kC.getVariable("a") != null)
+            System.out.print("The particle");
+        if (kC.getVariable("x0") != null) {
+            System.out.print(" starts at " + String.valueOf(kC.getVariable("x0")) + " meters");
+            useAnd = true;
+        }
+        if (kC.getVariable("v0") != null) {
+            if (useAnd)
+                System.out.print(",");
+            else
+                System.out.print(" is");
+            System.out.print(" moving at " + String.valueOf(kC.getVariable("v0")) + " meters per second");
+            useAnd = true;
+        }
+        if (kC.getVariable("a") != null) {
+            if (useAnd)
+                System.out.print(",");
+            else
+                System.out.print(" is");
+            System.out.print(" accelerating at a rate of " + String.valueOf(kC.getVariable("a")) + " meters per second squared");
+        }
+        if(kC.getVariable("x0") != null || kC.getVariable("v0") != null || kC.getVariable("a") != null)
+            System.out.print(".\n");
+
+        if(kC.getVariable("t") != null && (kC.getVariable("x") == null && kC.getVariable("v") == null))
+            System.out.print(String.valueOf(kC.getVariable("t")) + " seconds pass by");
+        else if (kC.getVariable("t") != null && (kC.getVariable("x") != null || kC.getVariable("v") != null)){
+            System.out.print("After " + String.valueOf(kC.getVariable("t")) + " seconds,");
+        }
+
+        if (kC.getVariable("t") == null && (kC.getVariable("x") != null || kC.getVariable("v") != null)){
+            System.out.print("After some time,");
+        }
+
+        useAnd = false;
+        if(kC.getVariable("x") != null){
+            System.out.print(" the particle is at " + String.valueOf(kC.getVariable("x")) + " meters");
+            useAnd = true;
+        }
+
+        if(kC.getVariable("v") != null){
+            if(useAnd)
+                System.out.print(", moving at " + String.valueOf(kC.getVariable("x")) + " meters per second");
+            else
+                System.out.print(" the particle is at " + String.valueOf(kC.getVariable("x")) + " meters per second");
+        }
+
+        if(kC.getVariable("x") != null || kC.getVariable("v") != null || kC.getVariable("t") != null)
+            System.out.print(".\n");
+
+    }
+}
